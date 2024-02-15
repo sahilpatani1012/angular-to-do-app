44f8808ceacec328fb6afce2909d3862ac89a95d
Sahil Patani <spatani9@gmail@gmail.com>
2024-02-15 15:33:16 +0530

    to-do items can be added to mongoDB

--- src/app/list-items.service.ts
+++ src/app/list-items.service.ts
@@ -11,23 +11,12 @@ export class ListItemsService {
 
   getListItems(userEmail:string): Observable<ToDoItem[]> {
     const url = `http://localhost:3000/api/${userEmail}`
-
     return this.http.get<ToDoItem[]>(url);
-    // let items: ToDoItem[] = [];
-    // const storedItems = localStorage.getItem("toDoItems");
-    // if (storedItems) {
-    //   items = JSON.parse(storedItems);
-    // }
-    // return items;
   }
 
   addListItem(item: ToDoItem, userEmail: string) {
     const url = `http://localhost:3000/api/${userEmail}/add-todos`
-
     return this.http.patch(url,item);
-    // let items: ToDoItem[] = this.getListItems();
-    // items.push(item);
-    // localStorage.setItem("toDoItems", JSON.stringify(items));
   }
 
   changeList(newList: ToDoItem[]) {
@@ -35,14 +24,5 @@ export class ListItemsService {
     let newListString: any = localStorage.getItem("toDoItems")
     return JSON.parse(newListString)
   }
-
-  toDoItems: ToDoItem[] = [
-    { id: 1, title: 'Buy groceries', completed: true, deadline: new Date('2024-03-17T03:24:00') },
-    { id: 2, title: 'Finish project report', completed: false, deadline: new Date('2024-11-20T14:48:00') },
-    { id: 3, title: 'Call the bank', completed: false, deadline: new Date('2024-07-09T09:36:00') },
-    { id: 4, title: 'Clean the house', completed: false, deadline: new Date('2024-05-22T16:12:00') },
-    { id: 5, title: 'Prepare for the meeting', completed: false, deadline: new Date('2024-08-30T21:18:00') }
-];
-
 
 }
--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -1,4 +1,4 @@
-import { Component,inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DataViewModule } from 'primeng/dataview';
 import { CommonModule } from '@angular/common';
 import { ToDoItem } from '../to-do-item';
@@ -10,34 +10,32 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-list-items',
   standalone: true,
-  imports: [DataViewModule,CommonModule,CheckboxModule,FormsModule],
+  imports: [DataViewModule, CommonModule, CheckboxModule, FormsModule],
   templateUrl: './list-items.component.html',
   styleUrl: './list-items.component.css'
 })
 export class ListItemsComponent {
 
-  constructor(private router:ActivatedRoute){}
+  constructor(private router: ActivatedRoute) { }
 
   private service = inject(ListItemsService);
-  toDoItems:ToDoItem[] = []
-  items : any = []
-  ngOnInit(){
-    const userEmail:any = this.router.snapshot.paramMap.get('userEmail');
+  toDoItems: ToDoItem[] = []
+  items: any = []
+  ngOnInit() {
+    const userEmail: any = this.router.snapshot.paramMap.get('userEmail');
     this.service.getListItems(userEmail)
       .subscribe({
-        next:(response:ToDoItem[]) => {
-          this.toDoItems = response
+        next: (response: ToDoItem[]) => {
+          this.toDoItems = response          
         },
-        error:(error) => {
+        error: (error) => {
           console.log(error);
         }
       })
-    // this.items = (localStorage.getItem("toDoItems"));
-    // this.toDoItems = JSON.parse(this.items);
   }
 
-  taskCompletedHandler(id:number){
-    this.toDoItems = this.toDoItems.filter((item)=> item.id !== id)
+  taskCompletedHandler(id: number) {
+    this.toDoItems = this.toDoItems.filter((item) => item.id !== id)
     this.toDoItems = this.service.changeList(this.toDoItems);
   }
 
